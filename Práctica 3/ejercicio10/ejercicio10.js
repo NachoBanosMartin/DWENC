32ee044d7f30f8066d307170bcf3ecd8c6284b49
Nacho Baños <ibannosm01@educarex.es>
2025-09-29 09:59:22 +0200

    subo

--- Práctica 3/ejercicio10/ejercicio10.js
+++ Práctica 3/ejercicio10/ejercicio10.js
@@ -3,17 +3,17 @@
 programa debe dar pistas de que el número introducido es Mayor o Menor al
 generado.
 */
-
 let num = Math.floor(Math.random() * 10) + 1;
 
-let numero = parseInt(prompt("Introduce un número del 1 al 10 para saber si has acertado: "));
 
-if (numero === num) {
-    alert("Has acertado");
-} else if (numero < num) {
-    alert("El número introducido es menor al generado");
-} else {
-    alert("El número introducido es mayor al generado");
-}
+    let numero = parseInt(prompt("Introduce un número del 1 al 10 para saber si has acertado: "));
+
+    if (numero === num) {
+        alert("Has acertado");
+    } else if (numero < num) {
+        alert("El número introducido es menor al generado");
+    } else {
+        alert("El número introducido es mayor al generado");
+    }
 
 console.log(num);
--- Práctica 3/ejercicio13/ejercicio13.js
+++ Práctica 3/ejercicio13/ejercicio13.js
@@ -4,11 +4,12 @@ solicitar el numero de horas que has trabajado en un mes, teniendo en cuenta que
 las horas se pagan a 10€.
 */
 
-let horas = parseFloat(prompt("Introduce las horas trabajadas durante el mes: "));
+let horas = prompt("Introduce las horas trabajadas durante el mes: ");
 let precioHora = 10.00;
+let multiplicacion = (horas * precioHora);
 
 if(!isNaN(horas)){
-    alert("Su sueldo de este mes es de: " + (horas * precioHora));
+    alert("Su sueldo de este mes es de: " + multiplicacion.toFixed(2));
 }else{
     alert("Introduce una cantidad válida");
 }
